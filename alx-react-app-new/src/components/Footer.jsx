7f82e8f5f878e6d69049d205ac6521e00e55c449
Chelsea-Adjei <ruby.adjei02@gmail.com>
2024-11-08 11:59:33 +0000

    styling added

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return(
+        <footer style={{ backgroundColor: 'pink', color: 'black' }}>
+            <p style={{ fontWeight: 'bold'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent () {
+    return (
+        <main>
+            <p style={{
+                fontSize: '22px', color: 'yellow',
+            }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
