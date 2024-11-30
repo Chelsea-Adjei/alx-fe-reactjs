d0aa47fbc6801c4b23f811784d13f0e5cfac7cc3
Chelsea-Adjei <ruby.adjei02@gmail.com>
2024-11-30 18:42:22 +0000

    new styling updated

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,15 @@
 function UserProfile() {
     return (
-      <div className="user-profile" bg-gray-100 md:p-8 sm:p-4  md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg>
-        <img src="https://via.placeholder.com/150" alt="User" rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto />
-        <h1 md:text-xl sm:text-lg text-blue-800 my-4>John Doe</h1>
-        <p text-gray-600 sm:text-sm md:text-base>Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <div className="user-profile bg-gray-100 md:p-8 sm:p-4
+      md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg
+      shadow-lg hover:shadow-xl ">
+        <img className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto hover:scale-110
+            transition-transform duration-300 ease-in-out"
+            src="https://via.placeholder.com/150"
+            alt="User"
+        />
+        <h1 className="md:text-xl sm:text-lg text-blue-800 hover:text-blue-500 my-4">John Doe</h1>
+        <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
