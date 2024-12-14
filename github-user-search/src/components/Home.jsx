69afe531cab6013f0b3af451065b71290ef37ad4
Chelsea-Adjei <ruby.adjei02@gmail.com>
2024-12-14 12:59:55 +0000

    new updates added

--- github-user-search/src/components/Home.jsx
+++ github-user-search/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Welcome to the Quiz App!</h1>;
+  return <h1 className="bg-yellow-500">Welcome to the Quiz App!</h1>;
 };
 
 export default Home;
